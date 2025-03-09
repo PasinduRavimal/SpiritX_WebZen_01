f31b42850d38840e6ea881a9a2e2a8a17e1723fd
Januli Nanayakkara <2022is063@stu.ucsc.cmb.ac.lk>
2025-03-09 13:46:36 +0530

    Add password strength validation to signup form

--- views/signup.php
+++ views/signup.php
@@ -32,6 +32,19 @@
     <title>Signup</title>
     <link rel="stylesheet" href="css/login.css">
     <link rel="stylesheet" href="css/signup.css">
+
+    <script>
+        function validatePassword() {
+            let password = document.getElementById("password").value;
+            let strengthBar = document.getElementById("strength-bar");
+            let strength = 0;
+            if (password.match(/[a-z]+/)) strength++;
+            if (password.match(/[A-Z]+/)) strength++;
+            if (password.match(/[0-9]+/)) strength++;
+            if (password.match(/[^a-zA-Z0-9]+/)) strength++;
+            strengthBar.value = strength;
+        }
+    </script>
 </head>
 
 <body>
@@ -47,16 +60,20 @@
         </div>
         <div class="form-group1">
             <label for="username">Username:</label>
-            <input type="text" id="username" name="username" required>
+            <input type="text" id="username" name="username" required oninput="this.setCustomValidity('')">
+            <span class="error"><?php echo $errors['username'] ?? ''; ?></span> <br>
         </div>
 
         <div class="form-group2">
             <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required>
+            <input type="password" id="password" name="password" required onkeyup="validatePassword()">
+            <progress id="strength-bar" value="0" max="4"></progress>
+            <span class="error"><?php echo $errors['password'] ?? ''; ?></span>
         </div>
         <div class="form-group3">
             <label for="password">Confirm password:</label>
-            <input type="password" id="password" name="confirm-password" required><br>
+            <input type="password" id="password" name="confirm-password" required>
+            <span class="error"><?php echo $errors['confirm_password'] ?? ''; ?></span><br>
         </div>
          
         <div class="form-group">
